026f65ebc65b0711e255ffc8c4e6dd765948d1dd
root <root@raspberrypi.(none)>
2014-12-25 22:17:35 +0800

    support rasPi wiring  digitalWrite by pinWrite message

--- pi/src/gpio/src/listener.cpp
+++ pi/src/gpio/src/listener.cpp
@@ -2,7 +2,7 @@
 #include "std_msgs/String.h"
 #include <wiringPi.h>
 #include "gpio/pinMode.h"
-
+#include "gpio/pinWrite.h"
 
 /*
 void chatterCallback(const std_msgs::String::ConstPtr& msg)
@@ -12,14 +12,23 @@ void chatterCallback(const std_msgs::String::ConstPtr& msg)
 
 void chatterCallback(const gpio::pinModePtr& msg)
 {
-  ROS_INFO("I heard: pin [%i]  I/O = [%i]", msg->pin, msg->output);
+  //ROS_INFO("I heard: pinMode [%i]  I/O = [%i]", msg->pin, msg->output);
+  pinMode(msg->pin, msg->output == 1 ? OUTPUT : INPUT );
+}
+
+void pinWriteCallback(const gpio::pinWritePtr& msg)
+{
+  //ROS_INFO("I heard: pinWrite [%i]  I/O = [%i]", msg->pin, msg->high);
+  digitalWrite(msg->pin, msg->high == 1 ? HIGH : LOW );
 }
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "gpioExecuteNode");
     ros::NodeHandle n;
-    ros::Subscriber sub = n.subscribe("gpioTopic", 100, chatterCallback);
+    ros::Subscriber sub = n.subscribe("gpioPinMode", 100, chatterCallback);
+    
+    ros::Subscriber sub2= n.subscribe("gpioPinWrite", 100, pinWriteCallback);
     wiringPiSetup();
 
     ros::spin();
