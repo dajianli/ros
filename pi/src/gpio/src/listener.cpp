21d596f474bd68a54cd5af34f389fdbaefa8ea09
root <root@raspberrypi.(none)>
2014-12-25 11:45:28 +0800

    support pinMode message callback in gpioExcutor_node

--- pi/src/gpio/src/listener.cpp
+++ pi/src/gpio/src/listener.cpp
@@ -1,9 +1,18 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include <wiringPi.h>
+#include "gpio/pinMode.h"
+ 
 
+/*
 void chatterCallback(const std_msgs::String::ConstPtr& msg)
 {
   ROS_INFO("I heard: [%s]", msg->data.c_str());
+}*/
+
+void chatterCallback(const gpio::pinModePtr& msg)
+{
+  ROS_INFO("I heard: pin [%i]  I/O = [%i]", msg->pin, msg->output);
 }
 
 int main(int argc, char **argv)
@@ -11,5 +20,7 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "gpioExecuteNode");
     ros::NodeHandle n;
     ros::Subscriber sub = n.subscribe("gpioTopic", 100, chatterCallback);
+    wiringPiSetup();
+
     ros::spin();
 }
